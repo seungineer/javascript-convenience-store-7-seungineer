fc8a82b1d63a05f027b6540b1fbe650ee0ce2773
조승우 <choseung97@gmail.com>
2024-11-09 03:04:09 +0900

    test(fileReader): `products.md` 문자열 parsing 결과를 확인하는 테스트를 추가한다
    해당 parsing 함수 테스트 코드에서는 products.md 파일의 컨텐츠를 한 줄에 한 배열씩 요소 별로 split 되었는지 테스트한다.

--- __tests__/fileReaderTest.js
+++ __tests__/fileReaderTest.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { readProductsFile } = require('../src/utils/fileReader');
 
-const PRODUCTS_FILE =
+const PRODUCTS_FILE_CONTENT =
   'name,price,quantity,promotion\n콜라,1000,10,탄산2+1,콜라,1000,10,null';
 const FILE_PATH = 'public/products.md';
 
@@ -9,10 +9,10 @@ jest.mock('fs');
 
 describe('fileReader 함수', () => {
   test('파일의 데이터를 문자열로 반환', () => {
-    fs.readFileSync.mockReturnValue(PRODUCTS_FILE);
+    fs.readFileSync.mockReturnValue(PRODUCTS_FILE_CONTENT);
 
     const result = readProductsFile(FILE_PATH);
-    expect(result).toBe(PRODUCTS_FILE);
+    expect(result).toBe(PRODUCTS_FILE_CONTENT);
   });
 
   test('파일이 존재하지 않을 때 [ERROR]로 시작하는 에러 발생', () => {
@@ -22,5 +22,17 @@ describe('fileReader 함수', () => {
     expect(() => readProductsFile(FILE_PATH)).toThrow(
       '[ERROR] products.md 파일을 찾을 수 없습니다.]',
     );
+  });
+});
+
+describe('parseData 함수 테스트', () => {
+  test('데이터를 배열로 parsing', () => {
+    const fileContent = PRODUCTS_FILE_CONTENT;
+
+    const result = parseData(fileContent);
+    expect(result).toEqual([
+      ['콜라', '1000', '10', '탄산2+1'],
+      ['콜라', '1000', '5', 'null'],
+    ]);
   });
 });
