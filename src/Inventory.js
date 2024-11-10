54279094934bf6064a099dc4b8c6794c3d238afb
조승우 <choseung97@gmail.com>
2024-11-10 14:11:53 +0900

    refactor(inventory): 10줄을 초과하는 재고 객체 변환 기능을 모듈화한다
    `initializeFromParsedData()`를 `createProdcut()`, `setQuantities()`, `setNormalQuantity()`, `setPromotionQuantity()`, `addOrUpdateProduct()`, `mergeProductQuantities()`로 모듈화한다.

--- src/Inventory.js
+++ src/Inventory.js
@@ -6,32 +6,62 @@ export default class {
   }
 
   initializeFromParsedData(parsedProducts) {
-    parsedProducts.forEach(([name, price, quantity, promotion]) => {
-      const product = {
-        price: parseInt(price, 10),
-        normalQuantity: promotion === 'null' ? parseInt(quantity, 10) : 0,
-        promotionQuantity: promotion !== 'null' ? parseInt(quantity, 10) : 0,
-        promotion: promotion === 'null' ? null : promotion,
-      };
-
-      if (this.#products.has(name)) {
-        // promotion 상태가 다른 name인 상품이 이미 재고에 존재하는 경우
-        const existingProduct = this.#products.get(name);
-        existingProduct.normalQuantity += product.normalQuantity;
-        existingProduct.promotionQuantity += product.promotionQuantity;
-      } else {
-        this.#products.set(name, product);
-      }
+    parsedProducts.forEach((originalProductData) => {
+      const product = this.createProduct(originalProductData);
+      const productName = originalProductData[0];
+      this.addOrUpdateProduct(productName, product);
     });
   }
 
+  createProduct([name, price, quantity, promotion]) {
+    const baseProduct = {
+      price: parseInt(price, 10),
+      normalQuantity: 0,
+      promotionQuantity: 0,
+      promotion: null,
+    };
+
+    return this.setQuantities(baseProduct, quantity, promotion);
+  }
+
+  setQuantities(baseProduct, quantity, promotion) {
+    if (promotion === 'null') {
+      return this.setNormalQuantity(baseProduct, quantity);
+    }
+    return this.setPromotionQuantity(baseProduct, quantity, promotion);
+  }
+
+  setNormalQuantity(baseProduct, quantity) {
+    return { ...baseProduct, normalQuantity: parseInt(quantity, 10) };
+  }
+
+  setPromotionQuantity(baseProduct, quantity, promotion) {
+    return { ...baseProduct, promotionQuantity: parseInt(quantity, 10), promotion };
+  }
+
+  addOrUpdateProduct(name, product) {
+    if (this.#products.has(name)) {
+      const updatedProduct = this.mergeProductQuantities(this.#products.get(name), product);
+      this.#products.set(name, updatedProduct);
+      return;
+    }
+    // #products에 name인 제품이 존재하지 않는 경우
+    this.#products.set(name, product);
+  }
+
+  mergeProductQuantities(existingProduct, newProduct) {
+    return {
+      ...existingProduct,
+      normalQuantity: existingProduct.normalQuantity + newProduct.normalQuantity,
+      promotionQuantity: existingProduct.promotionQuantity + newProduct.promotionQuantity,
+    };
+  }
+
   hasProduct(name) {
-    if (this.#products.has(name)) return true;
-    return false;
+    return this.#products.has(name);
   }
 
   getProduct(name) {
-    const product = this.#products.get(name);
-    return product;
+    return this.#products.get(name);
   }
 }
