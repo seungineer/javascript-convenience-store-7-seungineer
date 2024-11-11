634429166bd1c350158ae2cd31cf220c0e0d699f
조승우 <choseung97@gmail.com>
2024-11-11 19:39:04 +0900

    feat(OutputView): 재고 목록을 출력하는 기능 시작점을 추가한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import OutputView from './OutputView.js';
+import Inventory from './Inventory.js';
+import Promotion from './Promotion.js';
+import fileReader from './utils/fileReader.cjs';
+
+const { readProductsFile } = fileReader;
+
 class App {
-  async run() {}
+  async run() {
+    const inventory = new Inventory();
+    const parsedProducts = await readProductsFile('./public/products.md');
+    inventory.initializeFromParsedData(parsedProducts);
+
+    const promotion = new Promotion();
+    await promotion.initialize();
+
+    inventory.setPromotionData(promotion);
+
+    OutputView.printWelcomeMessage();
+    inventory.displayProductList();
+  }
 }
 
 export default App;
